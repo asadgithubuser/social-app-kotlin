fac3d6bf4e396652492518b1bbb79c2dc66af0d3
asadgitlab <Ahad@*.12.com>
2021-02-23 20:28:06 +0600

    search user-profile list and follow-following features are complete

---
+++ app/src/main/java/com/example/instagramkotlin/Adapter/UserAdapter.kt
@@ -0,0 +1,117 @@
+package com.example.instagramkotlin.Adapter
+
+import android.content.Context
+import android.text.TextUtils
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.annotation.NonNull
+import androidx.recyclerview.widget.RecyclerView
+import com.example.instagramkotlin.Models.User
+import com.example.instagramkotlin.R
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+import com.squareup.picasso.Picasso
+import kotlinx.android.synthetic.main.activity_user_tem.view.*
+
+class UserAdapter(private var mContext: Context, private var mUser: List<User>, private var isFragment: Boolean = false)
+    : RecyclerView.Adapter<UserAdapter.ViewHolder>(){
+
+    val firebaseUser: FirebaseUser? = FirebaseAuth.getInstance().currentUser
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): UserAdapter.ViewHolder {
+        val view = LayoutInflater.from(mContext).inflate(R.layout.activity_user_tem, parent, false)
+        return UserAdapter.ViewHolder(view)
+    }
+
+    override fun getItemCount(): Int {
+       return mUser.size
+    }
+
+    override fun onBindViewHolder(viewHolder: ViewHolder, position: Int) {
+        val user = mUser[position]
+
+        viewHolder.full_name.text = user.getFullName()
+        viewHolder.profession.text = user.getProfession()
+        Picasso.get().load(user.getImage()).placeholder(R.drawable.profile).into(viewHolder.p_image)
+
+        followFollowingDtatus(user.getUid(), viewHolder.follow_btn)
+
+        viewHolder.follow_btn.setOnClickListener {
+            if(viewHolder.follow_btn.text.toString() == "Follow"){
+                firebaseUser?.uid.let {onlineuser ->
+                    FirebaseDatabase.getInstance().reference
+                    .child("Follow").child(onlineuser.toString())
+                    .child("Following").child(user.getUid())
+                    .setValue(true).addOnCompleteListener{ task ->
+                        if (task.isSuccessful){
+                            FirebaseDatabase.getInstance().reference
+                            .child("Follow").child(user.getUid())
+                            .child("Followers").child(onlineuser.toString())
+                            .setValue(true).addOnCompleteListener { task ->
+
+                            }
+                        }
+                    }
+                }
+            }
+            else
+            {
+                firebaseUser?.uid.let { onlineUser ->
+                    FirebaseDatabase.getInstance().reference
+                    .child("Follow").child(onlineUser.toString())
+                    .child("Following").child(user.getUid())
+                    .removeValue().addOnCompleteListener { task ->
+                        if(task.isSuccessful){
+                            FirebaseDatabase.getInstance().reference
+                            .child("Follow").child(user.getUid())
+                            .child("Followers").child(onlineUser.toString())
+                            .removeValue().addOnCompleteListener {
+
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+
+
+    class ViewHolder(@NonNull itemView: View): RecyclerView.ViewHolder(itemView){
+        var full_name: TextView = itemView.findViewById(R.id.f_name)
+        var profession: TextView = itemView.findViewById(R.id.profession)
+        var p_image: ImageView = itemView.findViewById(R.id.prof_image)
+        var follow_btn: Button = itemView.findViewById(R.id.follow_btn_sa)
+    }
+
+    private fun followFollowingDtatus(uid: String, followBtn: Button) {
+        var followBtnRef = firebaseUser?.uid.let { onlineUser ->
+            FirebaseDatabase.getInstance().reference.child("Follow").child(onlineUser.toString())
+                    .child("Following")
+        }
+        followBtnRef.addValueEventListener(object : ValueEventListener{
+            override fun onCancelled(p0: DatabaseError) {
+
+            }
+
+            override fun onDataChange(p0: DataSnapshot) {
+                if(p0.child(uid).exists()){
+                    followBtn.text = "Following"
+                }else{
+                    followBtn.text = "Follow"
+                }
+            }
+
+        })
+    }
+
+}
---
+++ app/src/main/java/com/example/instagramkotlin/Models/User.kt
@@ -0,0 +1,74 @@
+package com.example.instagramkotlin.Models
+
+class User {
+    private var fullName: String = ""
+    private var profession: String = ""
+    private var email: String = ""
+    private var password: String = ""
+    private var aboutUrSelf: String = ""
+    private var image: String = ""
+    private var uid: String = ""
+
+    constructor()
+
+    constructor(fullName: String, profession: String, email: String, password: String, aboutUrSelf: String,
+    image: String, uid: String){
+        this.fullName = fullName
+        this.profession = profession
+        this.email = email
+        this.password = password
+        this.aboutUrSelf = aboutUrSelf
+        this.image = image
+        this.uid = uid
+    }
+
+    fun getFullName():String{
+        return fullName
+    }
+    fun setFullName(fullName: String){
+        this.fullName = fullName
+    }
+    fun getProfession():String{
+        return profession
+    }
+    fun getEmail():String{
+        return email
+    }
+    fun getPassword():String{
+        return password
+    }
+    fun getBio():String{
+        return aboutUrSelf
+    }
+    fun getImage():String{
+        return image
+    }
+    fun getUid():String{
+        return uid
+    }
+
+
+    fun setProfession(profession: String){
+        this.profession = profession
+    }
+    fun setEmail(email: String){
+        this.email = email
+    }
+    fun setPassword(password: String){
+        this.password = password
+    }
+    fun setBio(aboutUrSelf: String){
+        this.aboutUrSelf = aboutUrSelf
+    }
+    fun setImage(image: String){
+        this.image = image
+    }
+    fun setUid(uid: String){
+        this.uid = uid
+    }
+
+
+
+
+
+}
